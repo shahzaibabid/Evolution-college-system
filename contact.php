6647071cb5b2631bea903a3554370a5ca4d5a128
shahzaib abid <shahzaibabidsaeed@gmail.com>
2022-08-01 22:07:48 +0500

    frontend

---
+++ contact.php
@@ -0,0 +1,168 @@
+<!---/demosWTR/Freedom/29-11-2019/course-freedom-demo_Free/396094817/web/contact.html/demosWTR/Freedom/29-11-2019/course-freedom-demo_Free/396094817/web/index.htmlArrayEdit status----><!---/demosWTR/Freedom/29-11-2019/course-freedom-demo_Free/396094817/web/index.htmlselect product_id,template_id,template_title,template_name,featured_image,featured_image_server_id,
+full_image_server_id,full_image from template_down_details where starter_demo_url='/demosWTR/Freedom/29-11-2019/course-freedom-demo_Free/396094817/web/index.html' lat part:contact.html---><!DOCTYPE html>
+<html lang="en">
+
+<!-- Mirrored from demo.w3layouts.com/demosWTR/Freedom/29-11-2019/course-freedom-demo_Free/396094817/web/contact.html by HTTrack Website Copier/3.x [XR&CO'2014], Sat, 30 Jul 2022 18:52:10 GMT -->
+<!-- Added by HTTrack --><meta http-equiv="content-type" content="text/html;charset=UTF-8" /><!-- /Added by HTTrack -->
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Evolution College System</title>
+  <link rel="stylesheet" href="assets/css/style-freedom.css">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+</head>
+<body>
+<script src='../../../../../../../ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js'></script><script src="../../../../../../../m.servedby-buysellads.com/monetization.js" type="text/javascript"></script>
+<script>
+(function(){
+	if(typeof _bsa !== 'undefined' && _bsa) {
+  		// format, zoneKey, segment:value, options
+  		_bsa.init('flexbar', 'CKYI627U', 'placement:w3layoutscom');
+  	}
+})();
+</script>
+<script>
+(function(){
+if(typeof _bsa !== 'undefined' && _bsa) {
+	// format, zoneKey, segment:value, options
+	_bsa.init('fancybar', 'CKYDL2JN', 'placement:demo');
+}
+})();
+</script>
+<script>
+(function(){
+	if(typeof _bsa !== 'undefined' && _bsa) {
+  		// format, zoneKey, segment:value, options
+  		_bsa.init('stickybox', 'CKYI653J', 'placement:w3layoutscom');
+  	}
+})();
+</script>
+<!--<script>(function(v,d,o,ai){ai=d.createElement("script");ai.defer=true;ai.async=true;ai.src=v.location.protocol+o;d.head.appendChild(ai);})(window, document, "//a.vdo.ai/core/w3layouts_V2/vdo.ai.js?vdo=34");</script>-->
+<div id="codefund"><!-- fallback content --></div>
+<script src="../../../../../../../codefund.io/properties/441/funder.js" async="async"></script>
+
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src='https://www.googletagmanager.com/gtag/js?id=G-98H8KRKT85'></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-98H8KRKT85');
+</script>
+<meta name="robots" content="noindex">
+<body>
+	<!-- Demo bar start -->
+  <link rel="stylesheet" href="../../../../../../assests/css/font-awesome.min.css">
+
+
+
+<!-- Top Menu 1 -->
+<section class="w3l-top-menu-1">
+	<div class="top-hd">
+		<div class="wrapper">
+	<header class="d-grid grid-columns-2">
+		<ul class="top-head">
+				<li><a class="href" href="#">Our Social</a></li>
+			<li><a class="href" href="#"><span class="fa fa-facebook"></span></a></li>
+			<li><a class="href" href="#"><span class="fa fa-twitter"></span></a></li>
+			<li><a class="href" href="#"><span class="fa fa-instagram"></span></a></li>
+			<li><a class="href" href="#"><span class="fa fa-linkedin"></span></a></li>
+		</ul>
+	
+		<div class="accounts">
+				<li class="top_li"><a href="login.html"><span class="fa fa-user"></span> Login</a></li>
+				<li class="top_li"><a href="signup.html"><span class="fa fa-lock"></span> Register</a></li>
+		</div>
+	</header>
+</div>
+</div>
+</section>
+<!-- //Top Menu 1 -->
+  <!-- headers start -->
+
+  <?php
+include("header.php");
+?>
+<!-- header end -->
+  <script src="assets/js/jquery-3.3.1.min.js"></script>
+  <script>
+      $('#drop').change(function () {
+      if ($('#drop').is(":checked")) {
+      $('body').css('overflow', 'hidden');
+      } else {
+      $('body').css('overflow', 'auto');
+      }
+      });
+      </script>
+</section>
+<!-- Headers-4 block -->
+<!-- inner banner -->
+<section class="inner-banner-main">
+    <div class="about-inner">
+        <div class="wrapper">
+            
+            <ul class="breadcrumbs-custom-path">
+                <h3>Contact Us</h3>
+                <p>Our door is always open to solve your query.</p>
+            </ul>
+        </div>
+    </div>
+</section>
+<!-- //covers -->
+<!---728x90--->
+
+<!-- contacts -->
+<section class="w3l-contacts-9-main">
+       
+    <div class="contacts-9"  id="contact">
+        <div class="wrapper">
+                <div class="top-map">
+                     <div class="map-content-9">
+                            
+                        <form action="https://sendmail.w3layouts.com/submitForm" method="post">
+                            
+                                <input type="text" name="w3lName" id="w3lName"  placeholder="Name" required="">
+                                <input type="email" name="w3lSender" id="w3lSender" placeholder="Email" required="">
+                                <input type="text" name="w3lName" id="w3lName"  placeholder="Subject" required="">
+                                <textarea name="w3lMessage" id="w3lMessage" placeholder="Message" required=""></textarea>
+                             <button type="submit">Send</button>
+                        </form>
+                    </div>
+                    <div class="cont-details">
+                            <div class="map">
+                            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3620.9065711513867!2d67.07122101447703!3d24.832868502479908!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3eb33c6b03254785%3A0x1b2a58aef179afe2!2sAptech%20Defence%20Center!5e0!3m2!1sen!2s!4v1659370170772!5m2!1sen!2s" width="600" height="350" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                            </div>                        
+                        </div>
+            </div>
+        </div>
+    </div>
+    <center>
+    <div class="container mb-5 row">
+        <div class="col-md-3">
+            <h6>Address: Aptech DHA Phase-2</h6>
+        </div>
+        <div class="col-md-3">
+            <h6>City: Karachi</h6>
+        </div>
+        <div class="col-md-3">
+            <h6>Phone: +923323235615</h6>
+        </div>
+        <div class="col-md-3">
+            <h6>Email: ECSacademy@gmail.com</h6>
+        </div>
+    </div>
+    </center>
+</section>
+<!---728x90--->
+
+<!-- //contacts -->
+<?php
+    include("footer.php");
+?>
+
+</body>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+</html>
---
+++ index.php
@@ -0,0 +1,161 @@
+<html lang="en">
+
+<meta http-equiv="content-type" content="text/html;charset=UTF-8" /><!-- /Added by HTTrack -->
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Evolution College System </title>
+  <link rel="stylesheet" href="assets/css/style-freedom.css">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+</head>
+<body>
+<script src='../../../../../../../ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js'></script><script src="../../../../../../../m.servedby-buysellads.com/monetization.js" type="text/javascript"></script>
+<script>
+(function(){
+	if(typeof _bsa !== 'undefined' && _bsa) {
+  		// format, zoneKey, segment:value, options
+  		_bsa.init('flexbar', 'CKYI627U', 'placement:w3layoutscom');
+  	}
+})();
+</script>
+<script>
+(function(){
+if(typeof _bsa !== 'undefined' && _bsa) {
+	// format, zoneKey, segment:value, options
+	_bsa.init('fancybar', 'CKYDL2JN', 'placement:demo');
+}
+})();
+</script>
+<script>
+(function(){
+	if(typeof _bsa !== 'undefined' && _bsa) {
+  		// format, zoneKey, segment:value, options
+  		_bsa.init('stickybox', 'CKYI653J', 'placement:w3layoutscom');
+  	}
+})();
+</script>
+<!--<script>(function(v,d,o,ai){ai=d.createElement("script");ai.defer=true;ai.async=true;ai.src=v.location.protocol+o;d.head.appendChild(ai);})(window, document, "//a.vdo.ai/core/w3layouts_V2/vdo.ai.js?vdo=34");</script>-->
+<div id="codefund"><!-- fallback content --></div>
+<script src="../../../../../../../codefund.io/properties/441/funder.js" async="async"></script>
+
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src='https://www.googletagmanager.com/gtag/js?id=G-98H8KRKT85'></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-98H8KRKT85');
+</script>
+<meta name="robots" content="noindex">
+<body>
+
+  <link rel="stylesheet" href="../../../../../../assests/css/font-awesome.min.css">
+
+<!-- Top Menu  -->
+<section class="w3l-top-menu-1">
+	<div class="top-hd">
+		<div class="wrapper">
+	<header class="d-grid grid-columns-2">
+		<ul class="top-head">
+				<li><a class="href" href="#">Our Social</a></li>
+			<li><a class="href" href="#"><span class="fa fa-facebook"></span></a></li>
+			<li><a class="href" href="#"><span class="fa fa-twitter"></span></a></li>
+			<li><a class="href" href="#"><span class="fa fa-instagram"></span></a></li>
+			<li><a class="href" href="#"><span class="fa fa-linkedin"></span></a></li>
+		</ul>
+	
+		<div class="accounts">
+				<li class="top_li"><a href="login.html"><span class="fa fa-user"></span> Login</a></li>
+				<li class="top_li"><a href="signup.html"><span class="fa fa-lock"></span> Register</a></li>
+		</div>
+	</header>
+</div>
+</div>
+</section>
+<!-- //Top Menu  -->
+<!-- Banner image -->
+<img src="assets/images/banner.jpeg" width="100%" height="300vw">
+
+  <!-- headers start -->
+
+   <?php
+include("header.php");
+?>
+<!-- header end -->
+  <script src="assets/js/jquery-3.3.1.min.js"></script>
+  <script>
+      $('#drop').change(function () {
+      if ($('#drop').is(":checked")) {
+      $('body').css('overflow', 'hidden');
+      } else {
+      $('body').css('overflow', 'auto');
+      }
+      });
+      </script>
+</section>
+
+<!-- slider start -->
+<div class="row">
+    <div class="col-md-4 col-sm-12" style="margin: 0px;">
+        <div id="carouselExampleSlidesOnly" class="w-100 carousel slide" data-bs-ride="carousel">
+            <div class="carousel-inner w-100">
+              <div class="carousel-item active w-100">
+                <img src="assets/images/banner-1.jpg" class="d-block w-100" height="300vh" alt="...">
+              </div>
+              <div class="carousel-item w-100">
+                <img src="assets/images/banner-2.jpg" class="d-block w-100" height="300vh" alt="...">
+              </div>
+              <div class="carousel-item w-100">
+                <img src="assets/images/banner-3.jpg" class="d-block w-100" height="300vh"alt="...">
+              </div>
+            </div>
+          </div>
+    </div>
+
+    <div class="col-md-4 col-sm-12" style="margin: 0px;">
+        <div id="carouselExampleSlidesOnly" class="w-100 carousel slide" data-bs-ride="carousel">
+            <div class="carousel-inner w-100">
+              <div class="carousel-item active w-100">
+                <img src="assets/images/banner-1.jpg" class="d-block w-100" height="300vh" alt="...">
+              </div>
+              <div class="carousel-item w-100">
+                <img src="assets/images/banner-2.jpg" class="d-block w-100" height="300vh" alt="...">
+              </div>
+              <div class="carousel-item w-100">
+                <img src="assets/images/banner-3.jpg" class="d-block w-100" height="300vh" alt="...">
+              </div>
+            </div>
+          </div>
+    </div>
+
+    <div class="col-md-4 col-sm-12" style="margin: 0px;">
+        <div id="carouselExampleSlidesOnly" class="w-100 carousel slide" data-bs-ride="carousel">
+            <div class="carousel-inner w-100">
+              <div class="carousel-item active w-100">
+                <img src="assets/images/banner-1.jpg" class="d-block w-100" height="300vh" alt="...">
+              </div>
+              <div class="carousel-item w-100">
+                <img src="assets/images/banner-2.jpg" class="d-block w-100" height="300vh" alt="...">
+              </div>
+              <div class="carousel-item w-100">
+                <img src="assets/images/banner-3.jpg" class="d-block w-100" height="300vh" alt="...">
+              </div>
+            </div>
+          </div>
+    </div>  
+</div>
+<!-- //slider end -->
+
+
+<br><br>
+  <!-- footer start -->
+
+  <?php
+include("footer.php");
+?>
+<!-- footer end -->
+</body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+</html>
