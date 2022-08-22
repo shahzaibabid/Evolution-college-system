f28ac75a0814ca2f0f8f47a8fc96c51079a0fe1d
shahzaib abid <shahzaibabidsaeed@gmail.com>
2022-08-22 18:50:07 +0500

    aaa

---
+++ lms/studyscheme.php
@@ -0,0 +1,197 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Evolution College System</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+</head>
+<style>
+    .content{
+width: 80%;
+background-color: white;
+height:auto;
+border-style: solid;
+  border-color: black;
+}
+.a-box {
+  display: inline-block;
+  width: 240px;
+  text-align: center;
+}
+
+.img-container {
+    height: 230px;
+    width: 200px;
+    overflow: hidden;
+    border-radius: 0px 0px 20px 20px;
+    display: inline-block;
+}
+
+.img-container img {
+    transform: skew(0deg, -13deg);
+    height: 250px;
+    margin: -35px 0px 0px -70px;
+}
+
+.inner-skew {
+    display: inline-block;
+    border-radius: 20px;
+    overflow: hidden;
+    padding: 0px;
+    transform: skew(0deg, 13deg);
+    font-size: 0px;
+    margin: 30px 0px 0px 0px;
+    background: #c8c2c2;
+    height: 250px;
+    width: 200px;
+}
+
+.text-container {
+  box-shadow: 0px 0px 10px 0px rgba(0,0,0,0.2);
+  padding: 120px 20px 20px 20px;
+  border-radius: 20px;
+  background: #fff;
+  margin: -120px 0px 0px 0px;
+  line-height: 19px;
+  font-size: 14px;
+}
+
+.text-container h3 {
+  margin: 20px 0px 10px 0px;
+  color: #04bcff;
+  font-size: 18px;
+}
+.heading{
+    background: blue;
+    color: white;
+    width:100%;
+    height:50px;
+    border-radius:2px;
+    margin-top:5px;
+    padding-top:1px;
+}
+</style>
+<body>
+        
+  <?php
+include("header.php");
+?>
+<br><br><br>
+<div class="container">
+<div class="container content">
+<div class="heading">
+    <h1>&nbsp;&nbsp;Studying Coures</h1>
+</div>
+  <div class="row">
+    <div class="col-md-6 col-sm-12 border">
+    <div class="a-box">
+        <div class="img-container">
+          <div class="img-inner">
+            <div class="inner-skew">
+              <img src="https://images.unsplash.com/photo-1584791097929-aa825fe8e1e1?ixlib=rb-1.2.1&q=85&fm=jpg&crop=entropy&cs=srgb&ixid=eyJhcHBfaWQiOjE0NTg5fQ">
+            </div>
+          </div>
+        </div>
+        <div class="text-container">
+          <h3>Teacher Name</h3>
+          <div>
+           Subject Name
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="col-md-6 col-sm-12 border">
+    <div class="a-box">
+        <div class="img-container">
+          <div class="img-inner">
+            <div class="inner-skew">
+              <img src="https://images.unsplash.com/photo-1584791097929-aa825fe8e1e1?ixlib=rb-1.2.1&q=85&fm=jpg&crop=entropy&cs=srgb&ixid=eyJhcHBfaWQiOjE0NTg5fQ">
+            </div>
+          </div>
+        </div>
+        <div class="text-container">
+          <h3>Teacher Name</h3>
+          <div>
+           Subject Name
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="col-md-6 col-sm-12 border">
+    <div class="a-box">
+        <div class="img-container">
+          <div class="img-inner">
+            <div class="inner-skew">
+              <img src="https://images.unsplash.com/photo-1584791097929-aa825fe8e1e1?ixlib=rb-1.2.1&q=85&fm=jpg&crop=entropy&cs=srgb&ixid=eyJhcHBfaWQiOjE0NTg5fQ">
+            </div>
+          </div>
+        </div>
+        <div class="text-container">
+          <h3>Teacher Name</h3>
+          <div>
+           Subject Name
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="col-md-6 col-sm-12 border">
+    <div class="a-box">
+        <div class="img-container">
+          <div class="img-inner">
+            <div class="inner-skew">
+              <img src="https://images.unsplash.com/photo-1584791097929-aa825fe8e1e1?ixlib=rb-1.2.1&q=85&fm=jpg&crop=entropy&cs=srgb&ixid=eyJhcHBfaWQiOjE0NTg5fQ">
+            </div>
+          </div>
+        </div>
+        <div class="text-container">
+          <h3>Teacher Name</h3>
+          <div>
+           Subject Name
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="col-md-6 col-sm-12 border">
+    <div class="a-box">
+        <div class="img-container">
+          <div class="img-inner">
+            <div class="inner-skew">
+              <img src="https://images.unsplash.com/photo-1584791097929-aa825fe8e1e1?ixlib=rb-1.2.1&q=85&fm=jpg&crop=entropy&cs=srgb&ixid=eyJhcHBfaWQiOjE0NTg5fQ">
+            </div>
+          </div>
+        </div>
+        <div class="text-container">
+          <h3>Teacher Name</h3>
+          <div>
+           Subject Name
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="col-md-6 col-sm-12 border">
+    <div class="a-box">
+        <div class="img-container">
+          <div class="img-inner">
+            <div class="inner-skew">
+              <img src="https://images.unsplash.com/photo-1584791097929-aa825fe8e1e1?ixlib=rb-1.2.1&q=85&fm=jpg&crop=entropy&cs=srgb&ixid=eyJhcHBfaWQiOjE0NTg5fQ">
+            </div>
+          </div>
+        </div>
+        <div class="text-container">
+          <h3>Teacher Name</h3>
+          <div>
+           Subject Name
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+</div>  
+
+
+</body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+</html>   
