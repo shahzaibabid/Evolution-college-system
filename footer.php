abe8be01db5e4fb3b5c1cbe89d4755ac56c8f686
shahzaib abid <shahzaibabidsaeed@gmail.com>
2022-08-23 15:45:38 +0500

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,13 +1,69 @@
 <style>
 
 
-/* Dotted border */
-hr.dotted {
-  border-top: 3px dotted #bbb;
-}
+
 </style>
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
+<link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700,800,900" rel="stylesheet">
+	
+		<link rel="stylesheet" href="css/ionicons.min.css">
+		<link rel="stylesheet" href="css/style.css">
 
-<hr class="dotted">
+
+		<footer class="footer-06">
+			<div class="container">
+				
+				<div class="row pt-4">
+				
+					<div class="col-md-12 col-lg-12 ">
+						<div class="row w-100">
+							<div class="col-md-4  mb-4">
+								<h2 class="footer-heading">Pages</h2>
+								<ul class="list-unstyled">
+		              <li><a href="index.php" class="py-1 d-block"><span class="ion-ios-checkmark-circle-outline mr-2"></span>Home</a></li>
+		              <li><a href="about.php" class="py-1 d-block"><span class="ion-ios-checkmark-circle-outline mr-2"></span>About</a></li>
+		              <li><a href="contact.php" class="py-1 d-block"><span class="ion-ios-checkmark-circle-outline mr-2"></span>Contact_Us</a></li>
+		              <li><a href="signup.php" class="py-1 d-block"><span class="ion-ios-checkmark-circle-outline mr-2"></span>Register</a></li>
+                  <li><a href="login.php" class="py-1 d-block"><span class="ion-ios-checkmark-circle-outline mr-2"></span>Login</a></li>
+
+		            </ul>
+							</div>
+							<div class="col-md-4  mb-4 ">
+								<h2 class="footer-heading">Admission</h2>
+								<ul class="list-unstyled">
+		              <li><a href="admissionschedule.php" class="py-1 d-block"><span class="ion-ios-checkmark-circle-outline mr-2"></span>admission_schedule</a></li>
+		              <li><a href="feestructare.php" class="py-1 d-block"><span class="ion-ios-checkmark-circle-outline mr-2"></span>Fee_structare</a></li>
+		              <li><a href="coursecatelogue.php" class="py-1 d-block"><span class="ion-ios-checkmark-circle-outline mr-2"></span>course_catelogue</a></li>
+                  <li><a href="admissonform.php" class="py-1 d-block"><span class="ion-ios-checkmark-circle-outline mr-2"></span>Apply_online</a></li>	            
+                </ul>
+							</div>
+
+              <div class="col-md-4  mb-4 ">
+								<h2 class="footer-heading">Study Portal</h2>
+								<ul class="list-unstyled">
+                <li><a href="lms/lmslogin.php" class="py-1 d-block"><span class="ion-ios-checkmark-circle-outline mr-2"></span>Lms_login</a></li>
+                </ul>
+							</div>
+						
+              <div class="col-md-12 col-lg-12 order-md-last">
+						<div class="row justify-content-end">
+							<div class="col-md-12 col-lg-12 ">
+								
+								<p class="copyright">
+							  Copyright &copy; |All rights reserved by Evolution_College_System| <br> <i class="ion-ios-heart" aria-hidden="true">Designed & developed by Shahzaib & Charles</i> 
+							 
+							</div>
+						</div>
+					</div>
+
+						</div>
+					</div>
+				</div>
+			</div>
+		</footer>
 
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<script src="js/jquery.min.js"></script>
+    <script src="js/popper.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/main.js"></script>
